d1a27945d58c96c3807b3a160143da65f54d602b
Joshua K. Farin <kfar10@joshuas-macbook-pro.local>
2018-10-13 14:14:32 -0700

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Joshua K. Farin on 10/13/18.
+//  Copyright © 2018 Joshua K. Farin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
